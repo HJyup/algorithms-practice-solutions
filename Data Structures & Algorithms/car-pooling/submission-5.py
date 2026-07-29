311f871cbba31269ae2aa997029b8441f7621542
danyil butov <89708817+HJyup@users.noreply.github.com>
2026-07-29 23:32:31 +0100

    Add: car-pooling - submission-5

---
+++ Data Structures & Algorithms/car-pooling/submission-5.py
@@ -0,0 +1,16 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        sweep = []
+
+        for trip in trips:
+            sweep.extend([(trip[1], trip[0]), (trip[2], -trip[0])])
+
+        sweep.sort()
+        curr = 0
+
+        for _, cap in sweep:
+            curr += cap
+            if curr > capacity:
+                return False
+
+        return True
