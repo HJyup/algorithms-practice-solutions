a38c23fb555b2ccc92ec7f940b08dc7d05e87830
danyil butov <89708817+HJyup@users.noreply.github.com>
2026-07-31 18:50:04 +0100

    Add: average-waiting-time - submission-3

---
+++ Data Structures & Algorithms/average-waiting-time/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def averageWaitingTime(self, customers: List[List[int]]) -> float:
+        if not customers:
+            return 0
+
+        customers.sort()
+        ans = 0
+
+        time = customers[0][0]
+        for start, wait in customers:
+            time = max(start, time)
+
+            ans += (time + wait) - start
+            time += wait
+
+        return ans / len(customers)
